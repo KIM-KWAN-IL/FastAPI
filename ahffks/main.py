f916d4513412e7a033a46861d515c5b2387c4852
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2024-04-16 21:16:25 +0900

    FastAPI with mySQL on Docker

---
+++ ahffks/main.py
@@ -0,0 +1,31 @@
+import pymysql
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# 데이터베이스 연결 설정
+def get_db_connection():
+    return pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='5451',
+        db='gcumall',
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.get("/")
+def read_items():
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            # SQL 쿼리 실행
+            sql = "SELECT * FROM board"
+            cursor.execute(sql)
+
+            # 결과 가져오기
+            result = cursor.fetchall()
+            return result
+    finally:
+        # 데이터베이스 연결 종료
+        connection.close()
